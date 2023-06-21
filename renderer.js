b934c2a45584229156d2dae7a42da1d6817a7bf5
Gustavo Cysneiros <gcc.prof@protonmail.com>
2023-06-21 01:01:00 -0300

    Add 'add patient' button

--- renderer.js
+++ renderer.js
@@ -4,7 +4,13 @@ const saveButton = document.querySelector('button.save');
 const cancelButton = document.querySelector('button.cancel');
 
 addPatientButton.addEventListener('click', () => {
-
+    insertRow({
+        name: '',
+        totalSessions: '',
+        heldSessions: '',
+    });
+    setSaveBtnTriggerers(table.querySelector('tr:last-of-type'));
+    saveButton.removeAttribute('disabled');
 });
 
 saveButton.addEventListener('click', () => {
@@ -32,56 +38,66 @@ cancelButton.addEventListener('click', () => {
 
 window.electronAPI.onGetPatients((_event, patients) => {
     for (const patient of patients) {
-        const tableRow = document.createElement('tr');
-        const nameData = document.createElement('td');
-        const totalSessionsData = document.createElement('td');
-        const heldSessionsData = document.createElement('td');
-        const actionsData = document.createElement('td');
-        const nameInput = document.createElement('input');
-        const totalSessionsInput = document.createElement('input');
-        const heldSessionsInput = document.createElement('input');
-        const deleteButton = document.createElement('button');
-        nameInput.type = 'text';
-        nameInput.value = patient.name;
-        totalSessionsInput.type = 'number';
-        totalSessionsInput.min = '0'
-        totalSessionsInput.value = patient.totalSessions;
-        heldSessionsInput.type = 'number';
-        heldSessionsInput.min = '0';
-        heldSessionsInput.value = patient.heldSessions;
-        deleteButton.classList.add('delete-patient');
-        deleteButton.setAttribute('hidden', '');
-        deleteButton.innerText = '+';
-        deleteButton.addEventListener('click', () => {});
-        nameData.classList.add('name');
-        nameData.insertAdjacentElement('beforeend', nameInput);
-        totalSessionsData.classList.add('total-sessions');
-        totalSessionsData.insertAdjacentElement('beforeend', totalSessionsInput);
-        heldSessionsData.classList.add('held-sessions');
-        heldSessionsData.insertAdjacentElement('beforeend', heldSessionsInput);
-        actionsData.classList.add('actions');
-        actionsData.insertAdjacentElement('beforeend', deleteButton);
-        tableRow.insertAdjacentElement('beforeend', nameData);
-        tableRow.insertAdjacentElement('beforeend', totalSessionsData);
-        tableRow.insertAdjacentElement('beforeend', heldSessionsData);
-        tableRow.insertAdjacentElement('beforeend', actionsData);
-        table.querySelector('tbody').insertAdjacentElement('beforeend', tableRow);
+        insertRow(patient);
     }
-    setSaveBtnTriggerers(table.querySelector('tbody'));
+    const tableRows = table.querySelectorAll('tbody tr');
+    for (const tableRow of tableRows) {
+        setSaveBtnTriggerers(tableRow);
+    }
     generateTBodyCopy();
 });
+
+function insertRow(patient) {
+    const tableRow = document.createElement('tr');
+    const nameData = document.createElement('td');
+    const totalSessionsData = document.createElement('td');
+    const heldSessionsData = document.createElement('td');
+    const actionsData = document.createElement('td');
+    const nameInput = document.createElement('input');
+    const totalSessionsInput = document.createElement('input');
+    const heldSessionsInput = document.createElement('input');
+    const deleteButton = document.createElement('button');
+    nameInput.type = 'text';
+    nameInput.value = patient.name;
+    totalSessionsInput.type = 'number';
+    totalSessionsInput.min = '0'
+    totalSessionsInput.value = patient.totalSessions;
+    heldSessionsInput.type = 'number';
+    heldSessionsInput.min = '0';
+    heldSessionsInput.value = patient.heldSessions;
+    deleteButton.classList.add('delete-patient');
+    deleteButton.setAttribute('hidden', '');
+    deleteButton.innerText = '+';
+    deleteButton.addEventListener('click', () => {});
+    nameData.classList.add('name');
+    nameData.insertAdjacentElement('beforeend', nameInput);
+    totalSessionsData.classList.add('total-sessions');
+    totalSessionsData.insertAdjacentElement('beforeend', totalSessionsInput);
+    heldSessionsData.classList.add('held-sessions');
+    heldSessionsData.insertAdjacentElement('beforeend', heldSessionsInput);
+    actionsData.classList.add('actions');
+    actionsData.insertAdjacentElement('beforeend', deleteButton);
+    tableRow.insertAdjacentElement('beforeend', nameData);
+    tableRow.insertAdjacentElement('beforeend', totalSessionsData);
+    tableRow.insertAdjacentElement('beforeend', heldSessionsData);
+    tableRow.insertAdjacentElement('beforeend', actionsData);
+    table.querySelector('tbody').insertAdjacentElement('beforeend', tableRow);
+}
 
 function generateTBodyCopy() {
     table.insertAdjacentElement('beforeend', table.querySelector('tbody').cloneNode(true));
     table.querySelector('tbody:last-of-type').setAttribute('hidden', '');
-    setSaveBtnTriggerers(table.querySelector('tbody:last-of-type'));
+    const tableRows = table.querySelectorAll('tbody:last-of-type tr');
+    for (const tableRow of tableRows) {
+        setSaveBtnTriggerers(tableRow);
+    }
 }
 
-function setSaveBtnTriggerers(tableBody) {
-    const saveBtnTriggerers = tableBody.querySelectorAll('input, button.delete-patient');
+function setSaveBtnTriggerers(tableRow) {
+    const saveBtnTriggerers = tableRow.querySelectorAll('input, button.delete-patient');
     for (const saveBtnTriggerer of saveBtnTriggerers) {
         saveBtnTriggerer.addEventListener(
-            saveBtnTriggerer.nodeName === 'INPUT' ? 'change' : 'click',
+            saveBtnTriggerer.nodeName === 'INPUT' ? 'input' : 'click',
             () => saveButton.removeAttribute('disabled')
         );
     }
