db8430debb549c4e6f2581d191fff0410ae4551d
Gustavo Cysneiros <gcc.prof@protonmail.com>
2023-11-30 11:55:06 -0300

    Manage multiple care days for the same patient

--- renderer.js
+++ renderer.js
@@ -1,6 +1,6 @@
-const addPatientButton = document.querySelector('button.add-patient');
 const yearSelector = document.querySelector('select.year');
 const monthSelector = document.querySelector('select.month');
+const addPatientButton = document.querySelector('nav button.add');
 const table = document.querySelector('table');
 const saveButton = document.querySelector('button.save');
 const cancelButton = document.querySelector('button.cancel');
@@ -16,20 +16,11 @@ window.electronAPI.onGetPatients((_event, patients) => {
     loadPatientsTable(currentPatients);
 });
 
-addPatientButton.addEventListener('click', () => {
-    const patient = {
-        name: '',
-        careDay: '',
-        psychologicalAssessment: '',
-        totalSessions: '',
-        heldSessions: '',
-    };
-    addPatientRow(patient);
-});
-
 yearSelector.addEventListener('change', (e) => {
     updatePatients();
     year = Number(e.target.value);
+    month = 1;
+    monthSelector.value = '01';
     loadPatientsTable(currentPatients);
 });
 
@@ -37,6 +28,17 @@ monthSelector.addEventListener('change', (e) => {
     updatePatients();
     month = Number(e.target.value);
     loadPatientsTable(currentPatients);
+});
+
+addPatientButton.addEventListener('click', () => {
+    const patient = {
+        name: '',
+        careDays: [],
+        psychologicalAssessment: '',
+        totalSessions: '',
+        heldSessions: '',
+    };
+    addPatientRow(patient);
 });
 
 saveButton.addEventListener('click', () => {
@@ -67,15 +69,19 @@ function addPatientRow(patient) {
     const duePaymentData = document.createElement('td');
     const actionsData = document.createElement('td');
     const nameInput = document.createElement('input');
-    const careDayInput = document.createElement('input');
+    const careDayAddButton = document.createElement('button');
+    const careDayList = document.createElement('ol');
     const psychologicalAssessmentInput = document.createElement('input')
     const totalSessionsInput = document.createElement('input');
     const heldSessionsInput = document.createElement('input');
     const deleteButton = document.createElement('button');
     nameInput.type = 'text';
     nameInput.value = patient.name;
-    careDayInput.type = 'date';
-    careDayInput.value = patient.careDay;
+    careDayAddButton.innerText = '+';
+    careDayAddButton.setAttribute('title', 'Adicionar dia de atendimento');
+    careDayAddButton.classList.add('add');
+    careDayAddButton.addEventListener('click', () => addCareDayItem());
+    for (const careDay of patient.careDays) addCareDayItem(careDay)
     psychologicalAssessmentInput.type = 'checkbox';
     if (patient.psychologicalAssessment) psychologicalAssessmentInput.setAttribute('checked', '');
     psychologicalAssessmentInput.addEventListener('click', e => e.target.toggleAttribute('checked'));
@@ -85,14 +91,15 @@ function addPatientRow(patient) {
     heldSessionsInput.type = 'number';
     heldSessionsInput.min = '0';
     heldSessionsInput.value = patient.heldSessions;
-    deleteButton.classList.add('delete-patient');
+    deleteButton.classList.add('delete');
     deleteButton.setAttribute('title', 'Excluir paciente');
     deleteButton.innerText = '+';
     deleteButton.addEventListener('click', e => e.target.parentElement.parentElement.remove());
     nameData.classList.add('name');
     nameData.insertAdjacentElement('beforeend', nameInput);
-    careDayData.classList.add('care-day');
-    careDayData.insertAdjacentElement('beforeend', careDayInput);
+    careDayData.classList.add('care-days');
+    careDayData.insertAdjacentElement('beforeend', careDayAddButton);
+    careDayData.insertAdjacentElement('beforeend', careDayList);
     psychologicalAssessmentData.classList.add('psychological-assessment');
     psychologicalAssessmentData.insertAdjacentElement('beforeend', psychologicalAssessmentInput);
     totalSessionsData.classList.add('total-sessions');
@@ -109,18 +116,46 @@ function addPatientRow(patient) {
     tableRow.insertAdjacentElement('beforeend', duePaymentData);
     tableRow.insertAdjacentElement('beforeend', actionsData);
     table.querySelector('tbody').insertAdjacentElement('beforeend', tableRow);
+
+    function addCareDayItem(careDay) {
+        const careDayItem = document.createElement('li');
+        const careDayInput = document.createElement('input');
+        const careDayDeleteButton = document.createElement('button');
+        const min = year.toString() + '-' + ((month < 10 ? '0' : '') + month.toString()) + '-01';
+        const max = year.toString() + '-' + ((month < 10 ? '0' : '') + month.toString()) + '-' + lastDayOfMonth(month);
+        careDayInput.type = 'date';
+        careDayInput.value = careDay ?? min;
+        careDayInput.setAttribute('min', min);
+        careDayInput.setAttribute('max', max);
+        careDayDeleteButton.innerText = '+';
+        careDayDeleteButton.setAttribute('title', 'Excluir dia de atendimento');
+        careDayDeleteButton.classList.add('delete');
+        careDayDeleteButton.addEventListener('click', e => e.target.parentElement.remove());
+        careDayItem.insertAdjacentElement('beforeend', careDayInput);
+        careDayItem.insertAdjacentElement('beforeend', careDayDeleteButton);
+        careDayList.insertAdjacentElement('beforeend', careDayItem);
+
+        function lastDayOfMonth(month) {
+            return ( 
+                month === 1 || month === 3 || month === 5 || month === 7 || month === 8 || month === 10 || month === 12 ? '31' :
+                month === 4 || month === 6 || month === 9 || month === 11 ? '30' :
+                year % 4 === 0 && year % 100 != 0 ? '29' : '28'
+            );
+        }
+    }
 }
 
 function updatePatients() {
     const patients = [];
     const tableRows = table.querySelectorAll('tbody tr');
     for (const tableRow of tableRows) {
+        const careDays = [...tableRow.querySelectorAll('.care-days input')].map(input => input.value);
         patients.push({
             name: tableRow.querySelector('.name input').value,
-            careDay: tableRow.querySelector('.care-day input').value,
+            careDays: careDays,
             psychologicalAssessment: tableRow.querySelector('.psychological-assessment input').hasAttribute('checked'),
             totalSessions: tableRow.querySelector('.total-sessions input').value,
-            heldSessions: tableRow.querySelector('.held-sessions input').value, 
+            heldSessions: tableRow.querySelector('.held-sessions input').value,
         });
     }
     if (!currentPatients[year]) currentPatients[year] = [];
